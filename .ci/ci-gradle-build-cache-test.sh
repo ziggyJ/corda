27c5e2d05ff8a29e6d428896bb4de6b23cd357a6
josecoll <jose.coll@r3cev.com>
2018-09-14 18:40:43 +0100

    Scripted sub-tasks using regex matching on Corda directory structure layout

--- .ci/ci-gradle-build-cache-test.sh
+++ .ci/ci-gradle-build-cache-test.sh
@@ -1,94 +1,45 @@
 #!/bin/bash
 
-echo ":core:test"
-cd core
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
+kotlinModuleDirs=`find . -wholename **/test/kotlin -print`
+for t in ${kotlinModuleDirs}; do
+    s=`expr "$t" : '\(.*src\)'`
+    t=${s::${#s}-4}
+    u=${t:2}
+    moduleName=`echo $u | sed 's/\//:/g' | sed 's/docs:source://g' | sed 's/^testing://g'`
+    echo Kotlin module: ${moduleName}
+    if [ ${moduleName} = "node" ]; then
+        echo Skipping Kotlin module ${moduleName}
+        continue
+    fi
+    ./gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} ${moduleName}:clean ${moduleName}:test --build-cache ${PERFORM_GRADLE_SCAN}
+done
 
-echo ":serialization:test"
-cd ../serialization
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
+javaModuleDirs=`find . -wholename **/test/java -print`
+for t in ${javaModuleDirs}; do
+    s=`expr "$t" : '\(.*src\)'`
+    if [ ${#s} -le 4 ]; then
+        echo Skipping module directory ${t}
+        continue
+    fi
+    t=${s::${#s}-4}
+    u=${t:2}
+    moduleName=`echo $u | sed 's/\//:/g' | sed 's/docs:source://g' | sed 's/^testing://g'`
+    echo Java module: ${moduleName}
+    if [ ${moduleName} = "node" ]; then
+        echo Skipping Java module ${moduleName}
+        continue
+    fi
+    ./gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} ${moduleName}:clean ${moduleName}:test --build-cache ${PERFORM_GRADLE_SCAN}
+done
 
-echo ":node-api:test"
-cd ../node-api
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":client:rpc:test"
-cd ../client/rpc
-../../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":confidential-identities:test"
-cd ../../confidential-identities
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":finance:test"
-cd ../finance
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":client:jackson:test"
-cd ../client/jackson
-../../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":client:jfx:test"
-cd ../jfx
-../../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":testing:node-driver:test"
-cd ../../testing/node-driver
-../../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":testing:test-utils:test"
-cd ../test-utils
-../../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-# De-compose node tests
-echo ":node:test --tests net.corda.node.services.vault.*"
-cd ../../node
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.vault.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.statemachine.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.statemachine.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.transactions.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.transactions.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.schema.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.schema.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.persistence.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.persistence.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.network.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.network.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.keys.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.keys.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.identity.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.identity.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.events.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.events.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.config.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.config.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.services.*Test*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.services.F*Test* --tests net.corda.node.services.N*Test* --tests net.corda.node.services.R*Test* --tests net.corda.node.services.S*Test* --tests net.corda.node.services.T*Test* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.utilities.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.utilities.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.serialization.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.serialization.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.modes.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.modes.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.messaging.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.messaging.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.internal.*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.internal.* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
-
-echo ":node:test --tests net.corda.node.*Test*"
-../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean test --tests net.corda.node.C*Test* --tests net.corda.node.N*Test* --tests S*Test* --build-cache ${PERFORM_GRADLE_SCAN}; echo $?
+nodeModuleDirs=`find node/src/*/*/net/corda/node -type d -regex ".*/test/.*"`
+for t in ${nodeModuleDirs}; do
+    if [[ $t = *"resources"* ]]; then
+        echo Skipping module directory ${t}
+        continue
+    fi
+    s=`echo $t | sed -E 's/.*(java|kotlin)//'`
+    testPackageName=`echo ${s:1} | sed 's/\//./g'`
+    echo Node module test package: ${testPackageName}
+    ./gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} node:clean node:test --tests ${testPackageName} --build-cache ${PERFORM_GRADLE_SCAN}
+done
