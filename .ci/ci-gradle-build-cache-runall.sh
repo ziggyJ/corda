de2f6c3fdede53a8f287285050ac428eb05b066b
josecoll <jose.coll@r3cev.com>
2018-09-14 12:42:11 +0100

    Added master run script for Gradle Cache builds.

---
+++ .ci/ci-gradle-build-cache-runall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export GRADLE_BUILD_CACHE_URL="${GRADLE_BUILD_CACHE_URL:-http://localhost:5071/cache/}"
+export USE_GRADLE_DAEMON="${USE_GRADLE_DAEMON:-false}"
+export GRADLE_CACHE_DEBUG="${GRADLE_CACHE_DEBUG:-false}"
+export PERFORM_GRADLE_SCAN="${PERFORM_GRADLE_SCAN:---scan}"
+
+# cd %teamcity.build.checkoutDir%
+echo "Using Gradle Build Cache: $GRADLE_BUILD_CACHE_URL"
+
+.ci/ci-gradle-build-cache-test.sh
+.ci/ci-gradle-build-cache-integrationTest.sh
+.ci/ci-gradle-build-cache-smokeTest.sh
+.ci/ci-gradle-build-cache-samples.sh
+.ci/ci-gradle-build-cache-tools.sh
