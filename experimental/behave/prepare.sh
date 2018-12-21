1f67e23fb1d408fa42594e7204e570bc03063094
josecoll <jose.coll@r3cev.com>
2018-12-21 15:12:36 +0000

    Updated to reflect new finance jar split + correct copying of most recent version of a jar.

--- experimental/behave/prepare.sh
+++ experimental/behave/prepare.sh
@@ -4,13 +4,21 @@ set -x
 
 # Please ensure you run this script using source code (eg. GitHub master, branch or TAG) that reflects the version label defined below
 # For example:
-#   corda-master   => git clone https://github.com/corda/corda
-#   r3corda-master => git clone https://github.com/corda/enterprise
-VERSION=corda-master
-STAGING_DIR=~/staging
-CORDA_DIR=${STAGING_DIR}/corda/${VERSION}
+#   os-master   => git clone https://github.com/corda/corda
+#   enterprise-master => git clone https://github.com/corda/enterprise
+VERSION=master
+
+STAGING_DIR="${STAGING_ROOT:-$TMPDIR/staging}"
+echo "Staging directory: $STAGING_DIR"
+
+CORDA_DIR=${STAGING_DIR}/corda/os/${VERSION}
+echo "Corda staging directory: $CORDA_DIR"
+
 CORDAPP_DIR=${CORDA_DIR}/apps
+echo "CorDapp staging directory: $CORDAPP_DIR"
+
 DRIVERS_DIR=${STAGING_DIR}/drivers
+echo "Drivers staging directory: $DRIVERS_DIR"
 
 # Set up directories
 mkdir -p ${STAGING_DIR}
@@ -20,11 +28,12 @@ mkdir -p ${DRIVERS_DIR}
 
 # Copy Corda capsule into deps
 cd ../..
-./gradlew clean :node:capsule:buildCordaJar :finance:jar
-cp -v $(ls node/capsule/build/libs/corda-*.jar | tail -n1) ${CORDA_DIR}/corda.jar
+./gradlew clean :node:capsule:buildCordaJar :finance:contracts:jar :finance:workflows:jar
+cp -v $(ls -t node/capsule/build/libs/corda-*.jar | head -1) ${CORDA_DIR}/corda.jar
 
-# Copy finance library
-cp -v $(ls finance/build/libs/corda-finance-*.jar | tail -n1) ${CORDAPP_DIR}
+# Copy finance CorDapps (contracts and flows)
+cp -v $(ls -t finance/contracts/build/libs/corda-finance-*.jar | head -1) ${CORDAPP_DIR}
+cp -v $(ls -t finance/workflows/build/libs/corda-finance-*.jar | head -1) ${CORDAPP_DIR}
 
 # Download database drivers
 curl "https://search.maven.org/remotecontent?filepath=com/h2database/h2/1.4.196/h2-1.4.196.jar" > ${DRIVERS_DIR}/h2-1.4.196.jar
@@ -32,4 +41,4 @@ curl -L "http://central.maven.org/maven2/org/postgresql/postgresql/42.1.4/postgr
 
 # Build Network Bootstrapper
 ./gradlew tools:bootstrapper:jar
-cp -v $(ls tools/bootstrapper/build/libs/*.jar | tail -n1) ${CORDA_DIR}/network-bootstrapper.jar
+cp -v $(ls -t tools/bootstrapper/build/libs/*.jar | head -1) ${CORDA_DIR}/network-bootstrapper.jar
