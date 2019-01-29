503cc7938d370a73e17e0e96ac4cf8ea5fabd285
Dominic Fox <dominic.fox@r3.com>
2019-01-29 14:58:41 +0000

    Simplify API

--- core/src/main/kotlin/net/corda/core/context/ExecutionContext.kt
+++ core/src/main/kotlin/net/corda/core/context/ExecutionContext.kt
@@ -1,8 +1,24 @@
 package net.corda.core.context
 
-import kotlin.properties.ReadOnlyProperty
+import net.corda.core.utilities.contextLogger
+import java.lang.reflect.Method
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
+import kotlin.reflect.jvm.javaGetter
+
+typealias ReadOnlyProperties = Map<Method, Any>
+typealias WriteableProperties = MutableMap<Method, Any>
+
+open class ContextWrapper<R : Any, W : Any>(val reader: (ContextData) -> R, val writer: (ContextDataDelta, R) -> W) {
+
+    inline fun <V> current(block: R.() -> V): V =
+        ExecutionContext.withCurrent { read(this@ContextWrapper, block) }
+
+    inline fun <V> withExtended(extendBlock: W.() -> Unit, actionBlock: ExecutionContext.() -> V) {
+        ExecutionContext.withExtended({ write(this@ContextWrapper, extendBlock) }, actionBlock)
+    }
+
+}
 
 /**
  * Contains all of the data for an execution context.
@@ -12,23 +28,22 @@ sealed class ContextData {
     /**
      * Read a property from the context's data.
      */
-    abstract fun <T : Any> read(property: KProperty<T>): T?
+    abstract fun <T : Any> read(property: KProperty<T>, defaultValue: T? = null): T
+
+    abstract val history: Sequence<ReadOnlyProperties>
 
     /**
      * Create new [ContextData] which extends this data with new values.
      */
-    fun extend(newLocal: Map<String, Any?>): ContextData = ContextData.Populated(newLocal, this)
-
-    /**
-     * Create a [ContextDataDelta] which represents a set of changes to apply to [ContextData].
-     */
-    fun newDelta(): ContextDataDelta = ContextDataDelta(mutableMapOf(), this)
+    fun extend(newLocal: ReadOnlyProperties): ContextData = ContextData.Populated(newLocal, this)
 
     /**
      * [ContextData] with no values.
      */
     object Empty : ContextData() {
-        override fun <T : Any> read(property: KProperty<T>): T? = null
+        override fun <T : Any> read(property: KProperty<T>, defaultValue: T?): T =
+                defaultValue ?: throw IllegalArgumentException("No context value found for property $property")
+        override val history: Sequence<ReadOnlyProperties> = emptySequence()
     }
 
     /**
@@ -37,8 +52,11 @@ sealed class ContextData {
      * @param local The values set within the current context.
      * @param previous Values set within previous contexts.
      */
-    data class Populated(private val local: Map<String, Any?>, private val previous: ContextData): ContextData() {
-        override fun <T : Any> read(property: KProperty<T>): T? = local[property.name] as? T ?: previous.read(property)
+    data class Populated(private val local: ReadOnlyProperties, private val previous: ContextData): ContextData() {
+        override fun <T : Any> read(property: KProperty<T>, defaultValue: T?): T =
+                local[property.javaGetter] as? T ?: previous.read(property, defaultValue)
+
+        override val history: Sequence<ReadOnlyProperties> get() = previous.history + local
     }
 }
 
@@ -48,23 +66,33 @@ sealed class ContextData {
  * @param delta A mutable map of the values to be changed.
  * @param data The [ContextData] to which the changes will be applied.
  */
-data class ContextDataDelta(private val delta: MutableMap<String, Any?>, private val data: ContextData) {
+data class ContextDataDelta(private val delta: WriteableProperties) {
+
     /**
      * Read a value from the delta, or from the original context data if not present.
      */
-    fun <T : Any> read(property: KProperty<T>): T? = delta[property.name] as? T ?: data.read(property)
+    fun <T : Any> read(property: KProperty<T>): T? = delta[property.javaGetter] as? T
 
     /**
      * Write a value into the delta.
      */
     fun <T : Any> write(property: KProperty<T>, value: T) {
-        delta[property.name] = value
+        delta[property.javaGetter!!] = value
     }
 
     /**
      * Merge the delta into its source [ContextData], producing a new [ContextData] set.
      */
-    fun merge(): ContextData = data.extend(delta)
+    fun merge(data: ContextData): ContextData = data.extend(delta)
+
+    /**
+     * Create a delegate for reading/writing the value of this property out of a wrapped [ContextDataDelta].
+     *
+     * @param initializer An optional initializer that will be run on first read of the value, populating it
+     * if it is `null`.
+     */
+    fun <R : Any, T : Any> updating(property: KProperty<T>): ReadWriteProperty<R, T> =
+            ContextPropertyWriter(this, property)
 }
 
 /**
@@ -75,6 +103,9 @@ data class ContextDataDelta(private val delta: MutableMap<String, Any?>, private
 data class ExecutionContext(val data: ContextData) {
 
     companion object {
+
+        val logger = contextLogger()
+
         /**
          * An [ExecutionContext] initialised with empty [ContextData]
          */
@@ -99,11 +130,6 @@ data class ExecutionContext(val data: ContextData) {
         inline fun <R> withCurrent(block: ExecutionContext.() -> R): R = current.get().block()
 
         /**
-         * Obtain the current [ExecutionContext], mapping its [ContextData] into a typesafe wrapper, and do something with the wrapper.
-         */
-        inline fun <T : Any, R> withCurrent(wrapper: (ContextData) -> T, block: T.() -> R): R = withCurrent { read(wrapper, block) }
-
-        /**
          * Create an [ExecutionContext] whose [ContextData] has been modified using an [Extender], make it the current context, run the
          * provided block, and restore the original context afterwards.
          */
@@ -120,16 +146,27 @@ data class ExecutionContext(val data: ContextData) {
     }
 
     /**
+     * We cache readers, since readers themselves cache the results of lookups against the context data map.
+     */
+    private val readerCache = mutableMapOf<ContextWrapper<*, *>, Any>()
+
+    /**
+     * Obtain and cache a reader for a given [ContextWrapper].
+     */
+    fun <R : Any> getReader(wrapper: ContextWrapper<R, *>) =
+        readerCache.getOrPut(wrapper as ContextWrapper<*, *>) { wrapper.reader(data) } as R
+
+    /**
      * Wrap this context's [ContextData] in a typesafe wrapper, and read values from it via the wrapper.
      */
-    inline fun <T : Any, R> read(wrapper: (ContextData) -> T, block: T.() -> R): R =
-        wrapper(data).block()
+    inline fun <T : Any, R> read(wrapper: ContextWrapper<T, *>, block: T.() -> R): R =
+        getReader(wrapper).block()
 
     /**
      * Obtain a new [ExecutionContext] whose [ContextData] has been extended using an [Extender].
      */
     inline fun write(block: Extender.() -> Unit): ExecutionContext {
-        val extender = Extender(data.newDelta())
+        val extender = Extender(ContextDataDelta(mutableMapOf()), this)
         extender.block()
         return extender.createExtended()
     }
@@ -137,49 +174,33 @@ data class ExecutionContext(val data: ContextData) {
     /**
      * An [Extender] captures changes to an [ExecutionContext]'s [ContextData] via a [ContextDataDelta]
      */
-    class Extender(val delta: ContextDataDelta) {
+    class Extender(val delta: ContextDataDelta, val source: ExecutionContext) {
 
         /**
          * Add modified values to the [ContextDataDelta] via a wrapper.
          */
-        inline fun <T : Any> write(wrapper: (ContextDataDelta) -> T, configure: T.() -> Unit) {
-            wrapper(delta).configure()
+        inline fun <R : Any, W: Any> write(wrapper: ContextWrapper<R, W>, configure: W.() -> Unit) {
+            wrapper.writer(delta, source.getReader(wrapper)).configure()
         }
 
         /**
          * Obtain an [ExecutionContext] with the [ContextDataDelta] merged into its [ContextData].
          */
-        fun createExtended(): ExecutionContext = ExecutionContext(delta.merge())
+        fun createExtended(): ExecutionContext = ExecutionContext(delta.merge(source.data))
     }
-}
-
-/**
- * A delegate through which a property of a typesafe wrapper can be read from [ContextData].
- */
-class ContextPropertyReader<R : Any, T : Any>(private val data: ContextData, val defaultValue: T? = null): ReadOnlyProperty<R, T> {
-
-    private lateinit var actualValue: T
-
-    override fun getValue(thisRef: R, property: KProperty<*>): T =
-            if (::actualValue.isInitialized) actualValue
-            else (data.read(property as KProperty<T>) ?: defaultValue ?:
-                throw IllegalArgumentException("No context value found for property $property")).apply { actualValue = this }
 }
 
 /**
  * A delegate through which a property of a typesafe wrapper can be read from [ContextData], and written into a [ContextDataDelta].
  */
-class ContextPropertyWriter<R : Any, T : Any>(private val delta: ContextDataDelta, val defaultValue: T? = null): ReadWriteProperty<R, T> {
-
-    private lateinit var actualValue: T
+private class ContextPropertyWriter<R : Any, T : Any>(
+        private val delta: ContextDataDelta,
+        private val readerProperty: KProperty<T>): ReadWriteProperty<R, T> {
 
     override fun getValue(thisRef: R, property: KProperty<*>): T =
-            if (::actualValue.isInitialized) actualValue
-            else (delta.read(property as KProperty<T>) ?: defaultValue ?:
-            throw IllegalArgumentException("No context value found for property $property")).apply { actualValue = this }
+        delta.read(readerProperty) ?: readerProperty.call()
 
     override fun setValue(thisRef: R, property: KProperty<*>, value: T) {
-        delta.write(property as KProperty<T>, value)
-        actualValue = value
+        delta.write(readerProperty, value)
     }
 }
--- core/src/test/kotlin/net/corda/core/context/ExecutionContextTests.kt
+++ core/src/test/kotlin/net/corda/core/context/ExecutionContextTests.kt
@@ -6,86 +6,71 @@ import kotlin.test.assertEquals
 class ExecutionContextTests {
 
     // Enables us to store a set of tags in the execution context.
-    object ContextTag {
+    object ContextTags : ContextWrapper<ContextTags.Reader, ContextTags.Writer>(::Reader, ::Writer) {
+
         class Reader(data: ContextData) {
-            val tags: Set<String> by ContextPropertyReader(data, emptySet())
+            val tags: Set<String> = data.read(::tags, emptySet())
         }
 
-        class Writer(delta: ContextDataDelta) {
-            var tags: Set<String> by ContextPropertyWriter(delta, emptySet())
-
-            fun add(vararg newTags: String) { tags = tags.plus(newTags) }
-            fun remove(vararg toRemove: String) { tags = tags.minus(toRemove) }
+        class Writer(delta: ContextDataDelta, reader: Reader) {
+            var tags: Set<String> by delta.updating(reader::tags)
         }
     }
-
-    // Some convenience methods for reading and writing tags
-    private inline fun <R> ExecutionContext.readTags(block: ContextTag.Reader.() -> R): R = read(ContextTag::Reader, block)
-    private fun ExecutionContext.Extender.addTags(vararg tags: String) = write(ContextTag::Writer) { add(*tags) }
-    private fun ExecutionContext.Extender.removeTags(vararg tags: String) = write(ContextTag::Writer) { remove(*tags) }
-    private fun getCurrentTags() = ExecutionContext.withCurrent { readTags { tags } }
 
     @Test
     fun nestedContexts() {
         // Initialise a context with some tags
-        ExecutionContext.initialize { addTags("foo", "bar") }
+        ExecutionContext.initialize { write(ContextTags) { tags = setOf("foo", "bar") } }
 
         // Read the tags out of the current context
-        assertEquals(setOf("foo", "bar"), getCurrentTags())
+        assertEquals(setOf("foo", "bar"), ContextTags.current { tags })
 
         // Enter an extended context, writing more tags, and obtain the new tags from that context.
-        ExecutionContext.withExtended({ addTags("baz") }) {
-            assertEquals(setOf("foo", "bar", "baz"), getCurrentTags())
+        ContextTags.withExtended({ tags += "baz" }) {
+            assertEquals(setOf("foo", "bar", "baz"), ContextTags.current { tags })
 
             // Create a nested context, with one of the tags removed.
-            ExecutionContext.withExtended({ removeTags("bar") }) {
-                assertEquals(setOf("foo", "baz"), getCurrentTags())
+            ContextTags.withExtended({ tags -= "bar" }) {
+                assertEquals(setOf("foo", "baz"), ContextTags.current { tags })
             }
 
             // Outside of the nested context, all of the tags are present.
-            assertEquals(setOf("foo", "bar", "baz"), getCurrentTags())
+            assertEquals(setOf("foo", "bar", "baz"), ContextTags.current { tags })
         }
 
         // Now we are outside of the extended context, tags revert to their originally configured value
-        assertEquals(setOf("foo", "bar"), getCurrentTags())
+        assertEquals(setOf("foo", "bar"), ContextTags.current { tags })
     }
 
-    object Serialization {
+    object Serialization : ContextWrapper<Serialization.Reader, Serialization.Writer>(::Reader, ::Writer) {
+
         class Reader(data: ContextData) {
-            val classLoader: ClassLoader by ContextPropertyReader(data, ClassLoader.getSystemClassLoader())
-            val lenientCarpenter: Boolean by ContextPropertyReader(data, true)
+            val classLoader: ClassLoader = data.read(::classLoader, ClassLoader.getSystemClassLoader())
+            val lenientCarpenter: Boolean = data.read(::lenientCarpenter, false)
         }
 
-        class Writer(delta: ContextDataDelta) {
-            var classLoader: ClassLoader by ContextPropertyWriter(delta, ClassLoader.getSystemClassLoader())
-            var lenientCarpenter: Boolean by ContextPropertyWriter(delta, true)
+        class Writer(delta: ContextDataDelta, reader: Reader) {
+            var classLoader: ClassLoader by delta.updating(reader::classLoader)
+            var lenientCarpenter: Boolean by delta.updating(reader::lenientCarpenter)
         }
     }
-
-
-    // Some convenience methods for reading and writing serialization properties
-    private inline fun <R> ExecutionContext.readSerialization(block: Serialization.Reader.() -> R): R = read(Serialization::Reader, block)
-    private inline fun ExecutionContext.Extender.writeSerialization(block: Serialization.Writer.() -> Unit) = write(Serialization::Writer, block)
 
     @Test
     fun multipleContexts() {
         ExecutionContext.initialize {
-            addTags("serialization")
-
-            writeSerialization {
-                lenientCarpenter = false
-            }
+            write(ContextTags) { tags += "serialization" }
+            write(Serialization) { lenientCarpenter = false }
         }
 
         ExecutionContext.withExtended({
-            writeSerialization { lenientCarpenter = true }
-            addTags("lenient carpentry")
+            write(ContextTags) { tags += "lenient carpentry" }
+            write(Serialization) { lenientCarpenter = true }
         }) {
-            assertEquals(setOf("serialization", "lenient carpentry"), readTags { tags })
-            assertEquals(true, readSerialization { lenientCarpenter })
+            assertEquals(setOf("serialization", "lenient carpentry"), ContextTags.current { tags })
+            assertEquals(true, Serialization.current { lenientCarpenter })
         }
 
-        assertEquals(setOf("serialization"), getCurrentTags())
-        assertEquals(false, ExecutionContext.withCurrent { readSerialization { lenientCarpenter } })
+        assertEquals(setOf("serialization"), ContextTags.current { tags })
+        assertEquals(false, Serialization.current { lenientCarpenter })
     }
 }
